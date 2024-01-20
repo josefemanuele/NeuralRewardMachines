af84d185b6014a9f0dc93da8ef5222ac80a18421
Fra-Tsuna <francescoarg@live.it>
2024-01-20 17:41:19 +0100

    reward machines plots image env

--- plot.py
+++ plot.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+def pad_list(lst, l):
+    if len(lst) < l:
+        num_to_pad = l - len(lst)
+        padded_list = lst + [lst[-1]] * num_to_pad
+        return padded_list
+    else:
+        return lst
+
 def plot(dir_name, num_exp, formula = None, max_reward=100, type='rewards'):
     if type == 'rewards':
         results = []
@@ -10,6 +18,7 @@ def plot(dir_name, num_exp, formula = None, max_reward=100, type='rewards'):
             with open("{}/train_rewards_{}.txt".format(dir_name, exp), "r") as f:
                 lines = f.readlines()
             lines = [float(line.strip()) for line in lines]
+            lines = pad_list(lines, 10000)
             lines = np.convolve(lines, np.ones(100)/100, mode='valid')
             results.append(lines)
         results = np.array(results)
@@ -29,6 +38,7 @@ def plot(dir_name, num_exp, formula = None, max_reward=100, type='rewards'):
             with open("{}/image_classification_accuracy_{}.txt".format(dir_name, exp), "r") as f:
                 lines = f.readlines()
             lines = [float(line.strip()) for line in lines]
+            lines = pad_list(lines, 10000)
             lines = np.convolve(lines, np.ones(100)/100, mode='valid')
             results.append(lines)
         results = np.array(results)
@@ -47,8 +57,10 @@ def plot(dir_name, num_exp, formula = None, max_reward=100, type='rewards'):
             with open("{}/sequence_classification_accuracy_{}.txt".format(dir_name, exp), "r") as f:
                 lines = f.readlines()
             lines = [float(line.strip()) for line in lines]
+            lines = pad_list(lines, 10000)
             lines = np.convolve(lines, np.ones(100)/100, mode='valid')
             results.append(lines)
+        results = pad_list(results, 10000)
         results = np.array(results)
         df = None
         df = pd.DataFrame(results).melt()
