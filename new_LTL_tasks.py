266f20bdf5dc4cab19f9ec28d21632a2dafb2cf8
Fra-Tsuna <francescoarg@live.it>
2024-01-29 21:46:09 +0100

    single plots

--- new_LTL_tasks.py
+++ new_LTL_tasks.py
@@ -13,18 +13,3 @@ formulas.append(("(F c0) & (F c1) & (G (! c2))", 3, "task7: visit({0}, {1}) + gl
 formulas.append(("(F c0) & (F c1) & (G (! c2)) & (G(! c3))", 4, "task8: visit({0}, {1}) + glob_av({2}) + glob_av({3})".format(*items)))
 formulas.append(("F(c0 & F(c1)) & G (! c2)", 3, "task9: seq_visit({0}, {1}) + glob_av({2})".format(*items)))
 formulas.append(("F(c0 & F(c1)) & G (! c2) & G(! c3)", 4, "task10: seq_visit({0}, {1}) + glob_av({2}) + glob_av({3})".format(*items)))
-
-#TODO:
-#RM+A2C symb_env            (V)
-#RNN+A2C symb_env           (entro 28/12)
-#grounding+A2C symb_env     (entro 28/12)
-
-#RM+DDQN symb_env           (entro 28/12)
-#grounding+A2C symb_env     (entro 04/01)
-
-#RM+A2C image_env           (entro 04/01)
-#RNN+A2C image_env          (entro 04/01)
-#grounding+A2C image_env    (entro 11/01)
-
-#RM+DDQN image_env           (entro 11/01)
-#grounding+A2C image_env     (entro 11/01)
--- plot_final.py
+++ plot_final.py
@@ -68,13 +68,15 @@ def triple_plot(source_1, source_2, source_3, task_category, destination, num_ex
     env = GridWorldEnv(formulas[0], "rgb_array", "symbolic", use_dfa_state=False, train=False)
     max_reward = env.max_reward
 
-    results_1 = []
-    results_2 = []
-    results_3 = []
-
+    title_count = 0
     for idx, formula in enumerate(formulas):
         if idx in task_category:
             print(formula[2])
+            
+            results_1 = []
+            results_2 = []
+            results_3 = []
+
             path_1 = os.path.join(source_1, formula[2])
             path_2 = os.path.join(source_2, formula[2])
             path_3 = os.path.join(source_3, formula[2])
@@ -83,46 +85,50 @@ def triple_plot(source_1, source_2, source_3, task_category, destination, num_ex
                 with open("{}/train_rewards_{}.txt".format(path_1, exp), "r") as f:
                     lines_1 = f.readlines()
                 lines_1 = [float(line.strip()) for line in lines_1]
-                # lines_1 = pad_list(lines_1, 10000)
+                lines_1 = pad_list(lines_1, 10000)
                 lines_1 = np.convolve(lines_1, np.ones(100)/100, mode='valid')
                 results_1.append(lines_1)
 
                 with open("{}/train_rewards_{}.txt".format(path_2, exp), "r") as f:
                     lines_2 = f.readlines()
                 lines_2 = [float(line.strip()) for line in lines_2]
+                lines_2 = pad_list(lines_2, 10000)
                 lines_2 = np.convolve(lines_2, np.ones(100)/100, mode='valid')
                 results_2.append(lines_2)
 
                 with open("{}/train_rewards_{}.txt".format(path_3, exp), "r") as f:
                     lines_3 = f.readlines()
                 lines_3 = [float(line.strip()) for line in lines_3]
+                lines_3 = pad_list(lines_3, 10000)
                 lines_3 = np.convolve(lines_3, np.ones(100)/100, mode='valid')
                 results_3.append(lines_3)
-    
-    results_1 = np.array(results_1)
-    results_2 = np.array(results_2)
-    results_3 = np.array(results_3)
+        
+            results_1 = np.array(results_1)
+            results_2 = np.array(results_2)
+            results_3 = np.array(results_3)
 
-    df1 = None
-    df2 = None
-    df3 = None
+            df1 = None
+            df2 = None
+            df3 = None
 
-    df1 = pd.DataFrame(results_1).melt()
-    df2 = pd.DataFrame(results_2).melt()
-    df3 = pd.DataFrame(results_3).melt()
+            df1 = pd.DataFrame(results_1).melt()
+            df2 = pd.DataFrame(results_2).melt()
+            df3 = pd.DataFrame(results_3).melt()
 
-    sns.lineplot(x="variable", y="value", data=df1, label = "NRM+A2C")
-    sns.lineplot(x="variable", y="value", data=df2, label = "RNN+A2C")
-    sns.lineplot(x="variable", y="value", data=df3, label = "RM+A2C")
+            sns.lineplot(x="variable", y="value", data=df1, label = "NRM+A2C")
+            sns.lineplot(x="variable", y="value", data=df2, label = "RNN+A2C")
+            sns.lineplot(x="variable", y="value", data=df3, label = "RM+A2C")
 
-    plt.title("Map enviroment, second task class", fontsize=17)
-    plt.axhline(y=max_reward, color='r', linestyle='--')
-    plt.tick_params(axis='both', which='both', labelsize=12)
-    plt.xlabel("Episodes", fontsize=17)
-    plt.ylabel("Rewards", fontsize=17)
-    plt.legend(loc = "lower right", fontsize=16)
-    plt.savefig(destination+"/second_class_nrm_vs_rnn_vs_rm_map.png")
-    plt.clf()
+            plt.title(f"Map enviroment, task{title_count}", fontsize=17)
+            plt.axhline(y=max_reward, color='r', linestyle='--')
+            plt.tick_params(axis='both', which='both', labelsize=12)
+            plt.xlabel("Episodes", fontsize=17)
+            plt.ylabel("Rewards", fontsize=17)
+            plt.legend(loc = "lower right", fontsize=16)
+            plt.savefig(destination+f"/task{title_count}_nrm_vs_rnn_vs_rm_map.png")
+            plt.clf()
+
+            title_count+=1
 
 def plot_sequence(source_1, task_category, destination, num_exp):
 
@@ -164,14 +170,15 @@ def plot_sequence(source_1, task_category, destination, num_exp):
 SOURCE_PATH_RNN = "Results/RNN_MAP/"
 SOURCE_PATH_GROUND = "Results/GROUNDED_MAP"
 SOURCE_PATH_RM = "Results/RM_MAP"
-DESTINATION_PATH = "Plots/"
+DESTINATION_PATH = "Plots/Single_Tasks"
 
-TASKS_1_2_3_5 = [0, 1, 2, 4]
-TASKS_7_8_9_10 = [6, 7, 8, 9]
+TASKS = [0, 1, 2, 4, 6, 7, 8, 9]
+# TASKS_1_2_3_5 = [0, 1, 2, 4, 6, 7, 8, 9]
+# TASKS_7_8_9_10 = []
 
 if not os.path.exists(DESTINATION_PATH):
     os.makedirs(DESTINATION_PATH+"/")
 
 # plot(SOURCE_PATH_GROUND, SOURCE_PATH_RNN_LOC, TASKS_7_8_9_10, DESTINATION_PATH, 5)
 # plot_sequence(SOURCE_PATH_GROUND, TASKS_7_8_9_10, DESTINATION_PATH, 5)
-triple_plot(SOURCE_PATH_GROUND, SOURCE_PATH_RNN, SOURCE_PATH_RM, TASKS_7_8_9_10, DESTINATION_PATH, 5)
+triple_plot(SOURCE_PATH_GROUND, SOURCE_PATH_RNN, SOURCE_PATH_RM, TASKS, DESTINATION_PATH, 5)
