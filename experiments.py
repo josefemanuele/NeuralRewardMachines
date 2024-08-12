e6ae8cd4e647a1b46eef717183a126d5f702f0ee
Elena-Umili <59656842+Elena-Umili@users.noreply.github.com>
2024-08-12 17:29:57 +0200

    update exp script

--- experiments.py
+++ experiments.py
@@ -8,7 +8,7 @@ from RL.A2C import recurrent_A2C
 from plot import plot
 
 #flags
-absl.flags.DEFINE_string("METHOD", "rnn", "Method to test, one in ['rnn', 'vrm', 'rm'], default= 'rnn' ")
+absl.flags.DEFINE_string("METHOD", "rnn", "Method to test, one in ['rnn', 'nrm', 'rm'], default= 'rnn' ")
 absl.flags.DEFINE_string("ENV", "map_env", "Environment to test, one in ['map_env', 'image_env'], default= 'map_env' ")
 absl.flags.DEFINE_string("LOG_DIR", "Results/", "path where to save the results, default='Results/'")
 absl.flags.DEFINE_integer("NUM_EXPERIMENTS", 5, "num of runs for each test, default= 5")
@@ -29,7 +29,7 @@ def launch_experiments(path, formula, experiment, env_type, method):
 
     if method == 'rnn':
         use_dfa_state = False
-    elif method == 'vrm':
+    elif method == 'nrm':
         use_dfa_state = False
     elif method == 'rm':
         use_dfa_state = True
